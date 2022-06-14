e6daf8caaaeb5d0a50202611af280eb3cc19e9f5
sanathkumar26700 <53991484+sanathkumar26700@users.noreply.github.com>
2022-06-14 11:33:00 +0530

    Create video card component

---
+++ src/Components/Video Card/video-card.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+import { useNavigate, useLocation, Link } from "react-router-dom";
+import {useAuth} from '../../Context/authContext'
+
+import './video-card.css'
+
+const VideoCard = ({ video, cardDirection }) => {
+    
+    return (
+        		
+
+        <div className="card vertical">
+        <Link to='/video'>
+        <div className="card__product--img">
+            
+                <img className="img " src={video.thumbnailUrl} alt="video thumbnail"/>
+           
+            <span className="badge--text badge--grey">{video.videoLength}</span>
+        </div>
+        <div className="card__product--content">
+            <h2 className="card__product--title">{video.title}</h2>
+            <span className="card__product--category">{video.views} views</span>
+            <img className="avatar__round avatar--sm" src={video.channelImg} alt='channel logo'/>
+            <h4 className="card__product--company">{video.channelName} </h4>
+            <p className="card__product--description"> {video.description} </p>
+        </div>
+        </Link>   												
+    </div>	
+                                        
+
+
+    )
+}
+
+export default VideoCard;
