1c903f1d779df651053008ddf988d7bde06430ea
sanathkumar26700 <53991484+sanathkumar26700@users.noreply.github.com>
2022-06-18 23:51:00 +0530

    Update : Add like and watch-later pages

--- src/Pages/Likes/likes.jsx
+++ src/Pages/Likes/likes.jsx
@@ -1,6 +1,27 @@
+import {useUserData} from '../../Context/userDataContext'
+import DynamicVideoCard from '../../Components/Video Card/dynamic-videCard'
+import '../../Utilities/CSS/Utilities.css'
+
 function Likes() {
+
+  const {userData:{ likedData }} = useUserData()
+  document.title='Likes | viewtube'
+
   return (
-    <h1>Likes</h1>
+    <section className="main__body">
+    {likedData.length?
+    <>
+      <nav className="nav__container">
+        <h1 className="page--title">Liked {likedData.length ? `(${likedData.length})` : ''}</h1>
+      </nav>
+      <section className="video-list--section main__body">
+        {likedData.map(video => <DynamicVideoCard video={video}/>)}
+      </section>
+    </>
+    :
+    <h1 className='empty-list--text'>You haven't liked any videos yet 😕</h1>
+    }      
+    </section>
   )
 }
 
