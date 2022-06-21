87cd5d628f10b3ec1114c504632d18400db9886b
sanathkumar26700 <53991484+sanathkumar26700@users.noreply.github.com>
2022-06-22 02:48:15 +0530

    feat : playlist related features

--- src/Components/Video Card/dynamic-videoCard.jsx
+++ src/Components/Video Card/dynamic-videoCard.jsx
@@ -4,9 +4,10 @@ import {useUserData} from '../../Context/userDataContext'
 import {deleteFromHistoryData} from '../../Utilities/JS/dataHandlers/historyDataHandlers'
 import {deleteFromLikedData} from '../../Utilities/JS/dataHandlers/likedDataHandlers'
 import {deleteFromWatchLaterData} from '../../Utilities/JS/dataHandlers/watchLaterDataHandlers'
+import {deleteVideoFromPlaylistData} from '../../Utilities/JS/dataHandlers/playListDataHandlers'
 import './video-card.css'
 
-const DynamicVideoCard = ({ video }) => {
+const DynamicVideoCard = ({ video, playlist = null }) => {
 
     const {title, thumbnailUrl, videoLength, youtubeId } = video
     const {auth:{token}} = useAuth()
@@ -36,6 +37,10 @@ const DynamicVideoCard = ({ video }) => {
                 <span onClick={()=>deleteFromWatchLaterData(video, token, userDataDispatch)}>
                 <i className="fas  icon active--icon fa-list-alt"></i>
                 </span>}
+                {playlist &&
+                    <span onClick={()=>deleteVideoFromPlaylistData(playlist, video, token, userDataDispatch)}>
+                        <i className="fas fa-trash"></i>
+                    </span>}
             </div>           												
         </div>
     )
